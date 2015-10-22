2c0e7fae64180410a6c0edac0024db8890c466ef
杨帆 <hzyang_fan@corp.netease.com>
2015-10-22 10:35:32 +0800

    add test

--- test/public/javascript/test.js
+++ test/public/javascript/test.js
@@ -1,13 +1,20 @@
 $ajax({
+
     // GET
     //url: '/getList',
     //method: 'GET',
     //data: {'limit': 10, 'offset': 0},
+
     // POST
     url: '/add',
     method: 'POST',
     data: {'name': 'yangFan'},
+
+    // CALLBACK
     success: function (json) {
         console.log(json);
+    },
+    error: function () {
+        //ignore
     }
 });
